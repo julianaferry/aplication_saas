5bfecbb40ef1ebdc72ade0b4406cfc9e389dc551
julianaferreira <julianaferry@gmail.com>
2016-01-22 10:35:56 +0000

    Fix

---
+++ app/controllers/users/registration_controller.rb
@@ -0,0 +1,14 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]#User.first.plan_id=2 or 1
+        if resource.plan_id == 2
+          resource.save_with_payment
+        else
+          resource.save
+        end
+      end
+    end
+  end
+end
